b45b3b2b5b14c2f5bd80da59b1ae5c0e17b18c18
flyfloh <flo@openclimbingguide.org>
2013-11-04 12:58:45 +0100

    add regions as resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 OcgBackend::Application.routes.draw do
+  resources :regions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
