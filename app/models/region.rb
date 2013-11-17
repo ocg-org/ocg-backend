010a5a0af6161ff5a67aecf91d2f9a8b8060a608
flyfloh <flo@openclimbingguide.org>
2013-11-17 14:22:27 +0100

    add validator for region parents and childs
    this validator keeps regions from being their own parents or childs
    in the region tree.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,22 @@
+class TreeValidator < ActiveModel::Validator
+  def validate(record)
+    if record.parent_region == record
+      record.errors[:base] << I18n.t('invalid_parent')
+    end
+    record.child_regions.each do |child|
+      if child == record
+        record.errors[:base] << I18n.t('invalid_child')
+      end
+    end
+  end
+
+end
+
+
 class Region < ActiveRecord::Base
   belongs_to :parent_region, class_name: 'Region', inverse_of: :child_regions
   has_many :child_regions, class_name: 'Region',
     inverse_of: :parent_region, foreign_key: :parent_region_id
+
+  validates_with TreeValidator
 end
