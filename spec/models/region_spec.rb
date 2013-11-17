1b920f1d02db4056257a7147445b69c4eebf0e7f
flyfloh <flo@openclimbingguide.org>
2013-11-17 14:21:58 +0100

    add test for parent region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,8 +4,14 @@ describe "Region" do
 
   it "does not hold itself as child region" do
     r = FactoryGirl.create(:region)
-    r.parent_region = r
+    r.child_regions << r
     assert !r.save, "Region cannot hold itself as child"
+  end
+
+  it "does not hold itself as parent region" do
+    r = FactoryGirl.create(:region)
+    r.parent_region = r
+    assert !r.save, "Region cannot have itself as parent"
   end
 
   it "holds only rocks or regions" do
