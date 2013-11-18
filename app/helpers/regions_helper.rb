523f808d358e7ff430125b0f9c35e556b6f51c05
flyfloh <flo@openclimbingguide.org>
2013-11-18 15:02:55 +0100

    add helper for breadcrumbs and display breadcrumbs on regions#show

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -1,2 +1,19 @@
 module RegionsHelper
+  def breadcrumbs(region)
+    str = "<ol class=\"breadcrumb\">"
+    str += bc_rec(region.parent_region) unless region.parent_region.nil?
+    str += "<li class=\"active\">#{region.name}</li>"
+    str += "</ol>"
+    str
+  end
+
+  def bc_rec(region)
+    if region.parent_region.nil?
+      str = ""
+    else
+      str = bc_rec(region.parent_region)
+    end
+    str += "<li>#{link_to region.name, region}</li>"
+    str
+  end
 end
