dab3555ff82fbfb3dba5da335c9f3ef514efb12b
flyfloh <flo@openclimbingguide.org>
2013-11-25 23:47:06 +0100

    add rock validation to route

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,4 @@
 class Route < ActiveRecord::Base
   belongs_to :rock
+  validates :rock, presence: true
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe Route do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Route" do
+  it "does not save without a rock" do
+    r = Route.create
+    r.name = "route 1"
+    r.rock = nil
+    assert !r.save, "Route should not be saved without a rock"
+  end
 end
