15881bc691c68c26c8db2864537c5a003ecc2403
flyfloh <flo@openclimbingguide.org>
2013-11-25 23:50:45 +0100

    add a rock to the test

--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "Routes Controller", :type => :feature do
 
   it "displays information for a route" do
+    rock = FactoryGirl.create :rock
     r = FactoryGirl.create(:route, :name => "Snake dike",
+                           :rock => rock,
                            :description => "Follow me!",
                            :difficulty => 5)
     page.visit "/routes/#{r.id}"
